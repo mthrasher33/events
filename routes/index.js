55f1f9e83f2532f62aedb77ad6164557d47e9444
Matthew Thrasher <matthewth@marvin.com>
2017-06-09 09:39:01 -0500

    Clean up

--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,23 @@ router.get('/', function(req, res, next) {
 router.get('/facebook', function(req,res,next){
 	var pages = [230622880690991, 350502711478, 361509650872623];
 	var events = [];
-	var accessToken = 'EAABoVMNCYKQBAB0ZCkWWbbLhxPBZBH6ZCHJiy54eeymhEmyQGiL11ZCycRz8dtEFy4AaQ1aRCxhFE3jkXtnqMckGIFZCa8wyAatmeU9ZCnsKggAIfpWC3xMY9H73ntTU1XPDdPYMSDrcrillyMyx13mJoYLWEa5ZAbkRVUhwpAbZAIjHRjLg3WV6';
-	var url = 'https://graph.facebook.com/v2.9/230622880690991/events?&access_token=' + accessToken;
+	var accessToken = 'EAABoVMNCYKQBAPxphuYgk0qJoDNqItz5vJhgdPGaWvcZBuZBwFZC6nFS22sh4pkPaHm2cOAb3WEYboid5zCZCFTpI58iwX67L9lh0QBd2zKKwdi5hgtQfK3G33yIGSSJxrUb2iCDWIAfyoXUggnZCaAqPAubjuZA21l38OONPq5eb4GqZCcWhTgWKTAZBa2QgIYZD';
+	var url = 'https://graph.facebook.com/v2.9/296448873767813/events?&access_token=' + accessToken;
+	//var url = req.params.id ? 'https://graph.facebook.com/v2.9/'+ req.params.id+'/events?&access_token=' + accessToken : 'https://graph.facebook.com/v2.9/99758046345/events?&access_token=' + accessToken
 	//city of minneapolis government: 99758046345
 	//women's march: 230622880690991
 	//isaiah: 350502711478
 	//our revolution: 361509650872623
+	//logan park neighborhood association: 163447610383117
+	//audubon park neighborhood association: 296448873767813
+
 
 	getPageEventData(url, function(){
   		processEventAttendees(events, 0, function(){processEventInterested(events,0,function(){console.log('done'); res.send(events);})})//function(){console.log('done'); res.send(events)})
    	});
 
 	function processEventAttendees(array, index, callback){
-		console.log('processEventsArray called')
+		console.log('Getting attendees for ' + array[index].name)
 		getEventAttendees('https://graph.facebook.com/v2.9/'+array[index].id+'/attending?&access_token=' + accessToken, array[index], function(){
 	        if(++index === array.length) {
 	            callback();
@@ -34,7 +38,7 @@ router.get('/facebook', function(req,res,next){
 	}
 
 	function processEventInterested(array, index, callback){
-		console.log('processEventInterested called')
+		console.log('Getting interested for ' + array[index].name)
 		getEventInterested('https://graph.facebook.com/v2.9/'+array[index].id+'/interested?&access_token=' + accessToken, array[index], function(){
 	        if(++index === array.length) {
 	            callback();
@@ -75,7 +79,7 @@ router.get('/facebook', function(req,res,next){
 			if(!error && response.statusCode ==200){
 		 	
 				var resJSON = JSON.parse(body)
-	 			var next = resJSON.paging ? resJSON.paging.next : ''
+	 			var next = resJSON.paging ? resJSON.paging.next : false
 
 	 			for(var key in resJSON.data){
 	 				if(event.attendees){event.attendees.push(resJSON.data[key])}
@@ -100,7 +104,7 @@ router.get('/facebook', function(req,res,next){
 			if(!error && response.statusCode ==200){
 				 	
 				var resJSON = JSON.parse(body)
-	 			var next = resJSON.paging.next
+	 			var next = resJSON.paging ? resJSON.paging.next : false
 
 	 			for(var key in resJSON.data){
 	 				if(event.interested){event.interested.push(resJSON.data[key])}
